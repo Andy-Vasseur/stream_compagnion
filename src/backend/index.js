f8354e52e9f1070ab3ba374622b8cde4e42d4f01
Andy-Vasseur <andyvasseurpro@outlook.com>
2024-10-14 23:22:04 +0200

    :sparkles: added the nodejs to obs integration to the backend

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 
 const express = require('express')
 
+const { default: OBSWebSocket } = require('obs-websocket-js')
+const obs = new OBSWebSocket()
+
 const cors = require('cors')
 
 const router = require('./app/routers/index')
@@ -17,6 +20,22 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(router)
+
+const connect = async () => {
+  try {
+    const { obsWebSocketVersion, negotiatedRpcVersion } = await obs.connect(
+      `${process.env.OBS_HOST}:${process.env.OBS_PORT}`,
+      process.env.OBS_PASSWORD,
+      { rpcVersion: 1 }
+    )
+
+    return `Connected to server ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`
+  } catch (error) {
+    console.error('Failed to connect', error.code, error.message)
+  }
+}
+
+connect().then((value) => console.log(value))
 
 app.get('/ping', (req, res) => {
   res.json({ message: 'Connexion établie !', timestamp: new Date() })
